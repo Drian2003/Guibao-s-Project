7307e8184c9dacb267767d9689b6bfa69f67d70f
Guibao <123623422+Drian2003@users.noreply.github.com>
2023-04-02 21:56:36 +0800

    Add files via upload

---
+++ secondScript.php
@@ -0,0 +1,22 @@
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+	$firstname = $_POST["firstname"];
+	$lastname = $_POST["lastname"];
+	$email = $_POST["email"];
+	$password = $_POST["password"];
+	$confirm_password = $_POST["confirm_password"];
+	
+	echo "First Name: $firstname<br>";
+	echo "Last Name: $lastname<br>";
+	echo "Email: $email<br>";
+	echo "Password Length: " . strlen($password) . "<br>";
+	
+	if ($password == $confirm_password) {
+		echo "Password and Confirm Password match<br>";
+	} else {
+		echo "Password and Confirm Password do not match<br>";
+	}
+} else {
+	echo "Form data not submitted";
+}
+?>
